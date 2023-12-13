00429656b2f1ba30b85d1efb25648138a5ed7400
carQuis <carmen.quispe@ucb.edu.bo>
2023-12-13 16:38:06 -0400

    Multiplicador

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,35 @@
+// Importar funciones de otros archivos (asegúrate de que las rutas sean correctas)
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
+// Seleccionar elementos del DOM
 const first = document.querySelector("#primer-numero");
+const firt = document.querySelector('#primero');
+const secon = document.querySelector("#segundo");
 const second = document.querySelector("#segundo-numero");
+const formu = document.querySelector('#multiplicar-form');
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const resu = document.querySelector("#resultadoMul");
 
+// Agregar evento al formulario de sumar
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  // Utilizar la función sumar y actualizar el contenido del div
+  div.innerHTML = `<p>${sumar(firstNumber, secondNumber)}</p>`;
+});
+
+// Agregar evento al formulario de multiplicar
+formu.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  const firstNumber = Number.parseInt(firt.value);
+  const secondNumber = Number.parseInt(secon.value);
+
+  // Utilizar la función multiplicar y actualizar el contenido del div
+  resu.innerHTML = `<p>${multiplicar(firstNumber, secondNumber)}</p>`;
 });
